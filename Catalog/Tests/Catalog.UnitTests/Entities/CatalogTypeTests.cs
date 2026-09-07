91b276f912e5d31f72e795d8bf4140432e527eb3
alexnfeitozad <72011569+alexnfeitozad@users.noreply.github.com>
2026-09-07 08:41:14 -0300

    refactor: streamline Product entity by consolidating validation methods and enhancing UpdateDetails functionality; improve unit tests for comprehensive coverage

--- Catalog/Tests/Catalog.UnitTests/Entities/CatalogTypeTests.cs
+++ Catalog/Tests/Catalog.UnitTests/Entities/CatalogTypeTests.cs
@@ -1,25 +1,36 @@
-
 using Catalog.Domain.Entities;
 
-namespace Catalog.UnitTests.Entities
+namespace Catalog.UnitTests.Entities;
+
+public class CatalogTypeTests
 {
-    public class CatalogTypeTests
+    [Fact]
+    public void Constructor_WithValidData_AssignsIdentityAndName()
     {
-        [Fact]
-        public void Should_Create_CatalogType_With_Valid_Data()
-        {
-            var catalogType = new CatalogType(1, "Test Catalog Type");
+        var catalogType = new CatalogType(1, "Test Catalog Type");
 
-            Assert.NotNull(catalogType);
-            Assert.Equal(1, catalogType.Id);
-            Assert.Equal("Test Catalog Type", catalogType.Name);
-        }
+        Assert.Equal(1, catalogType.Id);
+        Assert.Equal("Test Catalog Type", catalogType.Name);
+    }
 
-        [Fact]
-        public void Should_Reject_Non_Positive_CatalogType_Id()
-        {
-            Assert.Throws<ArgumentException>(() => new CatalogType(0, "Test Catalog Type"));
-            Assert.Throws<ArgumentException>(() => new CatalogType(-1, "Test Catalog Type"));
-        }
+    [Theory]
+    [InlineData(0)]
+    [InlineData(-1)]
+    public void Constructor_WithNonPositiveId_ThrowsArgumentException(int id)
+    {
+        var exception = Assert.Throws<ArgumentException>(() => new CatalogType(id, "Test Catalog Type"));
+
+        Assert.Equal("id", exception.ParamName);
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData("   ")]
+    public void Constructor_WithInvalidName_ThrowsArgumentException(string? name)
+    {
+        var exception = Assert.Throws<ArgumentException>(() => new CatalogType(1, name!));
+
+        Assert.Equal("name", exception.ParamName);
     }
 }
