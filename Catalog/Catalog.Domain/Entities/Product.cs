5ec0a793350cad3e12b05b2ce71a5970d4551153
alexnfeitozad <72011569+alexnfeitozad@users.noreply.github.com>
2026-09-07 05:10:13 -0300

    feat: update Product entity to use Guid for Id and enhance validation logic

--- Catalog/Catalog.Domain/Entities/Product.cs
+++ Catalog/Catalog.Domain/Entities/Product.cs
@@ -7,19 +7,21 @@ namespace Catalog.Domain.Entities
 {
     public class Product
     {
-        public int Id { get; private set; }
-        public string Name { get; private set; }
+        public Guid Id { get; private set; }
+        public string Name { get; private set; } = string.Empty;
         public decimal Price { get; private set; }
-        public string Description { get; private set; }
-        public string PictureFileName { get; private set; }
+        public string Description { get; private set; } = string.Empty;
+        public string PictureFileName { get; private set; } = string.Empty;
         public int CatalogTypeId { get; private set; }
         public int CatalogBrandId { get; private set; }
 
 
-        public Product(int id, string name, decimal price, string description,
+        public Product(Guid id, string name, decimal price, string description,
          string pictureFileName, int catalogTypeId, int catalogBrandId)
         {
-            Id = id;
+            ValidateProduct(name, price, description, pictureFileName, catalogTypeId, catalogBrandId);
+
+            Id = new Guid();
             Name = name;
             Price = price;
             Description = description;
@@ -28,6 +30,29 @@ namespace Catalog.Domain.Entities
             CatalogBrandId = catalogBrandId;
         }
 
+        //regras de negocio Domain
+        // Exemplo: Validar que o nome do produto não esteja vazio
+        private void ValidateProduct(string name, decimal price, string description, string pictureFileName, int catalogTypeId, int catalogBrandId)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException("Product name cannot be empty.");
+            }
 
+            if (price < 0)
+            {
+                throw new ArgumentException("Product price cannot be negative.");
+            }
+
+            if (catalogTypeId <= 0)
+            {
+                throw new ArgumentException("Catalog type ID must be greater than zero.");
+            }
+
+            if (catalogBrandId <= 0)
+            {
+                throw new ArgumentException("Catalog brand ID must be greater than zero.");
+            }
+        }
     }
 }
